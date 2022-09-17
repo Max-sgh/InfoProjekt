b8bb48214b4cd872e700d27ee1b46b48014a168a
Max-sgh <maximilian-sangerhausen@gmx.de>
2022-09-17 12:07:22 +0200

    Anmeldebildschirm

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from crypt import methods
+from urllib import request
+from flask import Flask, redirect, url_for, render_template, send_from_directory, request
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="icons")
 
 @app.route('/')
 def home():
     return "it works!"
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["username"]
+        password = request.form["password"]
+        #
+        # Anmeldung durchführen
+        #
+        return redirect(url_for("home"))
+    else:
+        return render_template("login.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(
+
+    )
